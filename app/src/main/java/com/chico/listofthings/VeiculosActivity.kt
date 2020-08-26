fa962554ee64a592de4c284d6e785becf21f5cce
fcpbarros <69065679+fcpbarros@users.noreply.github.com>
2020-08-25 23:05:51 -0300

    Commit #3
    Minor changes to commit #2

--- app/src/main/java/com/chico/listofthings/VeiculosActivity.kt
+++ app/src/main/java/com/chico/listofthings/VeiculosActivity.kt
@@ -30,28 +30,57 @@ class VeiculosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_veiculos)
 
+        pegaIDs()
+
         novo_veiculo.setOnClickListener {
             AdicionaVeiculoDialog(viewDaActivity as ViewGroup, this)
                 .adicionaVeiculo { novoVeiculo ->
-                    if (veiculos.contains(novoVeiculo)) {
-                        Toast.makeText(this, "Esse veículo já existe", Toast.LENGTH_LONG).show()
-                    } else {
-                        db.collection(COLECAO).document(novoVeiculo.id!!)
-                            .set(novoVeiculo)
-                            .addOnSuccessListener {
-                                Log.d(
-                                    "Teste",
-                                    "Documento adicionado no Firestore com o ID " + novoVeiculo.toString()
-                                )
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w("Teste", "Error adding document", e)
-                            }
-                    }
+                    adicionaVeiculoFirestore(novoVeiculo)
 
                 }
 
         }
+    }
+
+    private fun adicionaVeiculoFirestore(novoVeiculo: Veiculo) {
+        if (listaId.contains(novoVeiculo.id)) {
+            Toast.makeText(
+                this, "Esse veículo já existe",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            db.collection(COLECAO).document(novoVeiculo.id!!)
+                .set(novoVeiculo)
+                .addOnSuccessListener {
+                    Toast.makeText(
+                        this, "Veiculo adicionado:" + novoVeiculo.id,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(
+                        this, "Erro ao adicionar o veículo. " +
+                                "Por favor, checar a conexão com a internet",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+        }
+    }
+
+
+    private fun pegaIDs() {
+        db.collection(COLECAO)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val amostra = document.toObject(Veiculo::class.java)
+                    Log.d("testeID", "${amostra.id}")
+                    listaId.add(amostra.id!!)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Teste", "Error getting documents: ", exception)
+            }
     }
 
 }
